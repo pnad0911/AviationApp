0e3f7e6d6141ed5266e58d1574fd4ab33208c6f3
pnad0911 <pnad0911@gmail.com>
2018-01-28 17:35:39 -0800

    Update A19015.java

--- src/A19015.java
+++ src/A19015.java
@@ -9,9 +9,6 @@ import java.util.Scanner;
  * @author Duy Pham
  * @version 1.01 - 05/23/2014
  * @email pnad0911@gmail.com
- * @prgm.usage A19015
- * @assignment.number A19015
- * @see <a href='http://jcouture.net/cisc190/A19015.php'>Program Specification</a>
  * @see <br><a href='http://docs.oracle.com/javase/7/docs/technotes/guides/Javadoc/index.html'>Javadoc Documentation</a>
  */
 
